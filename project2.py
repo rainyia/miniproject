21d9ab001dde7486a477b6fcb4d6d58b6e60f015
Lorraine Torres <92608657+rainyia@users.noreply.github.com>
2024-02-02 17:42:49 -0800

    Add files via upload
    project2.py - Rock, paper, scissors. This console game permits the user to enter rock, paper or scissors and play against the computer, which randomly generates rock, paper, or scissors. The game isn't currently designed to loop. This is version 1.

---
+++ project2.py
@@ -0,0 +1,37 @@
+import random
+
+# Rock, Paper, Scissors
+
+
+def get_computer_move(): #random choice generator for computer
+    choices = ['rock', 'paper', 'scissors']
+    return random.choice(choices)
+
+
+def get_player_move(): #player input logic
+    choice = input("Enter rock, paper, or scissors: ").lower()
+    while choice not in ['rock', 'paper', 'scissors']:
+        choice = input("Invalid choice. please enter rock, paper, or scissors.")
+    return choice
+
+
+def the_winner(user_choice, computer_choice): #win lose conditions
+    if user_choice == computer_choice:
+        return "It's a tie!"
+    elif (user_choice == "rock" and computer_choice == "scissors") \
+        or (user_choice == "scissors" and computer_choice == "paper") \
+        or (user_choice == "paper" and computer_choice == "rock"):
+        return "You Win!"
+    else:
+        return "You Lose!"
+
+
+def play():
+    user_choice = get_player_move()
+    computer_choice = get_computer_move()
+    print(f"\nYou chose: {user_choice}, The computer chose: {computer_choice}")
+    result = the_winner(user_choice, computer_choice)
+    print(result)
+
+if __name__ == "__main__":
+    play()
